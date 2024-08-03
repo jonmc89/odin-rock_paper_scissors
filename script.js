df1c4166995b2e6ec2533496d9c6b88bd6c98b41
jonmc89 <126274870+jonmc89@users.noreply.github.com>
2024-08-03 16:19:41 +0100

    Play Round and Play Game added

--- script.js
+++ script.js
@@ -6,17 +6,56 @@ function getComputerChoice() {
   switch (computerChoice) {
     case 1:
       return "rock";
-      break;
     case 2:
       return "paper";
-      break;
     case 3:
       return "scissors";
-      break;
   }
 }
 
 function getHumanChoice() {
   let humanChoice = prompt("Choose rock, paper or scissors?");
-  return humanChoice.toLocaleLowerCase();
+  return humanChoice.toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    return "You Tied";
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    humanScore++;
+    return "You Win";
+  } else {
+    computerScore++;
+    return "You Lose";
+  }
+}
+
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    const result = playRound(humanChoice, computerChoice);
+
+    console.log(`Round ${i + 1}`);
+    console.log(`You chose: ${humanChoice}`);
+    console.log(`Computer chose: ${computerChoice}`);
+    console.log(result);
+    console.log(`Human Score: ${humanScore}`);
+    console.log(`Computer Score: ${computerScore}`);
+    console.log("-------------------------");
+  }
+
+  if (humanScore > computerScore) {
+    console.log("Congratulations! You won the game!");
+  } else if (humanScore < computerScore) {
+    console.log("Sorry, you lost the game.");
+  } else {
+    console.log("The game is a tie!");
+  }
+}
+
+playGame();
